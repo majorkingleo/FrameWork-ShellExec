d79dede9cfe195132175a298d304e2a369ca333b
kingleo <kingleo@0809fef9-18a3-0410-bae6-c9758b01f9bc>
2010-12-03 17:48:24 +0000

    Das Plugin registriert sich nun selbst��ndig als UrlOpen Handler (Unter Windows)
    git-svn-id: https://svn.borger.co.at/repos/redeye/ShellExec/Main/trunk@1288 0809fef9-18a3-0410-bae6-c9758b01f9bc

--- at/redeye/Plugins/ShellExec/Plugin.java
+++ at/redeye/Plugins/ShellExec/Plugin.java
@@ -7,23 +7,28 @@ package at.redeye.Plugins.ShellExec;
 
 import at.redeye.FrameWork.base.AutoMBox;
 import at.redeye.FrameWork.base.Root;
+import at.redeye.FrameWork.base.Setup;
 import at.redeye.FrameWork.widgets.helpwindow.HelpFileLoader;
+import at.redeye.FrameWork.widgets.helpwindow.HyperlinkExecuter;
+import at.redeye.FrameWork.widgets.helpwindow.OpenUrlInterface;
 
 /**
  *
  * @author martin
  */
-public class Plugin implements at.redeye.FrameWork.Plugin.Plugin
+public class Plugin implements at.redeye.FrameWork.Plugin.Plugin, OpenUrlInterface
 {    
     public static String NAME = "ShellExec";
     
     Root root;
 
+    @Override
     public String getName()
     {
         return NAME;
     }
 
+    @Override
     public String getLicenceText()
     {
         final StringBuilder builder = new StringBuilder();
@@ -43,17 +48,25 @@ public class Plugin implements at.redeye.FrameWork.Plugin.Plugin
         return builder.toString();
     }
 
+    @Override
     public void initPlugin(Object obj) 
     {
         if( obj != null  && obj instanceof Root )
         {
             root = (Root) obj;
             root.addDllExtractorToCache(new HSWChellExecDLL());
+
+            if( Setup.is_win_system() )
+                HyperlinkExecuter.setOpenUrl(this);
         }
     }
 
+    @Override
     public boolean isAvailable() {
-        return true;
+        if( Setup.is_win_system() )
+            return true;
+
+        return false;
     }
 
     @Override
@@ -62,6 +75,7 @@ public class Plugin implements at.redeye.FrameWork.Plugin.Plugin
         return getName() + " " + getVersion();
     }
 
+    @Override
     public String getChangeLog() {
 
         final StringBuilder builder = new StringBuilder();
@@ -81,7 +95,15 @@ public class Plugin implements at.redeye.FrameWork.Plugin.Plugin
         return builder.toString();
     }
 
+    @Override
     public String getVersion() {
         return Version.getVersion();
+    }
+
+    @Override
+    public void openUrl(String url)
+    {
+        ShellExec shell = new ShellExec();
+        shell.execute(url);
     }
 }
